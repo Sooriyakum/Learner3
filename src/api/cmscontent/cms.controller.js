cf8dcabb6516ed882ccb59daa659a8044416ec19
Sooriyakum <74105617+Sooriyakum@users.noreply.github.com>
2020-11-11 23:08:07 -0800

    Update cms.controller.js

--- src/api/cmscontent/cms.controller.js
+++ src/api/cmscontent/cms.controller.js
@@ -66,9 +66,52 @@ const getFreedom = async (req, res, next) => {
     next(error);
   }
 };
+const addMaster = (req,res,next) =>{
+try{
+  let tablename = req.params.tablename;
+  let body = req.body;
+  console.log(body)
+  res.send("body")
+}catch(error){
+console.log(error)
+}
+}
 
+
+
+const sendmail = (req,res,next) =>{
+var sender = nodemailer.createTransport(
+{
+  host: "0.0.0.0",
+  port: 25255,
+service:'gmail',
+auth:
+{
+user:'xx368528@gmail.com',
+pass:'maha@1999'
+}
+});
+
+var composemail ={
+from:'xx368528@gmail.com',
+to:'mahalakshmid50@gmail.com',
+subject:'maha',
+text:'hi'
+};
+
+sender.sendMail(composemail,function(error,info){
+if(error)
+{
+console.log(error);
+}
+else{
+console.log("mail sent successsfully"+info.response);
+}
+});
+  res.send("Success")
+};
 module.exports = {
   sandboxtest,
-  getFreedom
+  getFreedom,addMaster,sendmail
 };
 
